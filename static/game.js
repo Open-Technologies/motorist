9a58c4a29742ee03dffbfa8abd12147336295b2e
dima <blyus@adkernel.com>
2015-08-06 02:19:14 +0300

    Initial map editor

--- static/game.js
+++ static/game.js
@@ -1,24 +1,49 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+document.addEventListener('click', function () {
+  raycaster.setFromCamera(mouse, camera);
+  var nPos = new THREE.Vector3(0, 0, 0);
+  var n = new THREE.Vector3(0, 0, 1);
+  var rayPos = raycaster.ray.origin.clone();
+  var ray = raycaster.ray.direction.clone();
+  var t = - rayPos.sub(nPos).dot(n) / ray.dot(n);
+  var p = rayPos.add(ray.multiplyScalar(t));
+  p.x = Math.floor(p.x + 0.5);
+  p.y = Math.floor(p.y + 0.5);
+  console.log(p);
+  addPlane(p);
+}, false);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var materials = {
+  GTATIL1_204: new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('textures/GTATIL1_204.bmp')})
+};
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+function addPlane(pos) {
+  var object = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 1, 1), materials.GTATIL1_204);
+  object.position.copy(pos);
+  scene.add(object);
+}
+
 
 camera.position.z = 5;
 
 var render = function () {
-    requestAnimationFrame( render );
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-    renderer.render(scene, camera);
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
 };
+
+function onDocumentMouseMove(event) {
+  event.preventDefault();
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = -( event.clientY / window.innerHeight ) * 2 + 1;
+}
 
 render();
