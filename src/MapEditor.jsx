57acccee4fa0613f1e3dc6da8ba73b985a30116b
dima <blyus@adkernel.com>
2015-08-12 03:40:56 +0300

    Tiles can be placed

--- src/MapEditor.jsx
+++ src/MapEditor.jsx
@@ -6,41 +6,55 @@ const MapEditor = React.createClass({
   getInitialState() {
     return {
       render: null,
-      rollOver: null
+      rollOverPosition: null,
+      rollOverObject: null
     };
   },
   componentDidMount() {
     this.state.render = new Render();
     this.state.render.camera.position.z = 5;
+    this.state.render.domElement.addEventListener('click', this.onClick);
     this.state.render.domElement.addEventListener('mousemove', this.onMouseMove);
     this.state.render.domElement.addEventListener('mouseout', this.onMouseOut);
   },
   componentWillUnmount() {
+    this.state.render.domElement.removeEvenListener('click', this.onClick);
     this.state.render.domElement.removeEvenListener('mousemove', this.onMouseMove);
     this.state.render.domElement.removeEvenListener('mouseout', this.onMouseOut);
   },
+  onClick(e) {
+    this.updateRollOverPosition(e);
+    const newTile = this.state.render.addTile({
+      material: materials.GTATIL1_204
+    });
+    newTile.position.copy(this.state.rollOverPosition);
+  },
   onMouseMove(e) {
-    if (!this.state.rollOver) {
+    if (!this.state.rollOverObject) {
       // Create if not exist
       const material = materials.GTATIL1_204.clone();
       material.opacity = 0.5;
       material.transparent = true;
-      this.state.rollOver = this.state.render.addTile({material});
+      this.state.rollOverObject = this.state.render.addTile({material});
     }
 
     // Update position
-    const point = this.state.render.getIntersectionWithZ(e.clientX, e.clientY, 0);
-    point.x = Math.floor(point.x + 0.5);
-    point.y = Math.floor(point.y + 0.5);
-    this.state.rollOver.position.copy(point);
+    this.updateRollOverPosition(e);
+    this.state.rollOverObject.position.copy(this.state.rollOverPosition);
   },
   onMouseOut() {
     // Remove rollOver
-    if (!this.state.rollOver) {
+    if (!this.state.rollOverObject) {
       return;
     }
-    this.state.render.removeObject(this.state.rollOver);
-    this.state.rollOver = null;
+    this.state.render.removeObject(this.state.rollOverObject);
+    this.state.rollOverObject = null;
+  },
+
+  updateRollOverPosition(e) {
+    this.state.rollOverPosition = this.state.render.getIntersectionWithZ(e.clientX, e.clientY, 0);
+    this.state.rollOverPosition.x = Math.floor(this.state.rollOverPosition.x + 0.5);
+    this.state.rollOverPosition.y = Math.floor(this.state.rollOverPosition.y + 0.5);
   },
 
   render() {
