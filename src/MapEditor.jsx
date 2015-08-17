5b6aaed71b630df2b70449176ac5f963d6c6b1ed
dima <blyus@adkernel.com>
2015-08-18 01:29:39 +0300

    Add new ground tiles

--- src/MapEditor.jsx
+++ src/MapEditor.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import Render from './Render';
 import materials from './materials';
 
+const tiles = [
+  'GTATIL1_197', 'GTATIL1_198', 'GTATIL1_199', 'GTATIL1_200', 'GTATIL1_201', 'GTATIL1_202', 'GTATIL1_204',
+  'GTATIL1_206', 'GTATIL1_209', 'GTATIL1_212', 'GTATIL1_219', 'GTATIL1_220', 'GTATIL1_221', 'GTATIL1_233',
+  'GTATIL1_234', 'GTATIL1_247', 'GTATIL1_250', 'GTATIL1_254', 'GTATIL1_265', 'GTATIL1_266', 'GTATIL1_270',
+  'GTATIL1_271', 'GTATIL1_272', 'GTATIL1_274', 'GTATIL1_275', 'GTATIL1_276', 'GTATIL1_277', 'GTATIL1_278',
+  'GTATIL1_279', 'GTATIL1_280', 'GTATIL1_289', 'GTATIL1_291', 'GTATIL1_294', 'GTATIL1_295', 'GTATIL1_303',
+  'GTATIL1_304', 'GTATIL1_315', 'GTATIL1_316', 'GTATIL1_322', 'GTATIL1_323', 'GTATIL1_329', 'GTATIL1_330',
+  'GTATIL1_331', 'GTATIL1_338'
+];
+
 const MapEditor = React.createClass({
   getInitialState() {
     return {
@@ -60,7 +70,10 @@ const MapEditor = React.createClass({
   render() {
     return (
       <div className="tileMenu">
-        <img className="tileMenu__tile" src="images/textures/GTATIL1_204.bmp"/>
+        {tiles.map((tileName, key) => {
+          const src = 'images/textures/' + tileName + '.bmp';
+          return <img className="tileMenu__tile" src={src} key={key}/>;
+        })}
         <div className="clear"/>
       </div>
     );
